98d2faf303abb31155af5758abff25c6e972c8ce
William Porter <dawilster143@gmail.com>
2012-07-22 19:52:19 +1000

    prod env vars

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,14 +5,14 @@ Gist::Application.configure do
   config.cache_classes = false
 
   # Full error reports are disabled and caching is turned on
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
   # Compress JavaScripts and CSS
-  config.assets.compress = false
+  config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
