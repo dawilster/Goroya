43c56eb96870a7e5c5f83b8e682020b042fdb286
William Porter <dawilster143@gmail.com>
2012-07-22 15:36:02 +1000

    first commit

---
+++ app/controllers/fetch_controller.rb
@@ -0,0 +1,70 @@
+class FetchController < ApplicationController
+	include HTTParty
+	require 'RDiscount'
+
+	def index
+
+		@gist = Post.find('3155667')
+		@content = @gist.content			
+		@desc = @gist.name		
+		render :action => 'show'
+	end
+
+	def show
+		if params[:up] == 'update'
+
+			@response = HTTParty.get("https://api.github.com/gists/#{params[:id]}")		
+			@gist = Post.find(params[:id])	
+			@gist.name = @response["description"]		
+			@response["files"].each do |file|
+				@content = file[1]["content"]
+			end		
+			@gist.content = RDiscount.new(@content).to_html
+			@gist.id = params[:id]
+			@gist.gist_id = @response["user"]["login"]
+			if @gist.save	
+				# flash[:message] = "updated gist"		
+			end		
+		end
+		if  Post.exists?(params[:id])
+			@gist = Post.find(params[:id])
+			@content = @gist.content			
+			@desc = @gist.name
+			@user = @gist.gist_id
+		else
+			@response = HTTParty.get("https://api.github.com/gists/#{params[:id]}")
+			if @response["message"]
+				render :file => "#{Rails.root}/public/404.html"
+			else	
+				@gist = Post.new
+				@gist.name = @desc = @response["description"]
+				@response["files"].each do |file|
+					@content = file[1]["content"]
+				end
+				@gist.content = @content = RDiscount.new(@content).to_html
+				@gist.id = params[:id]
+				@gist.gist_id = @response["user"]["login"]
+				@gist.save
+			end
+		end
+
+	end
+
+	def not_found
+
+	end
+
+	private
+
+	def update_gist(id)
+		gist = Post.find(id)
+		response = HTTParty.get("https://api.github.com/gists/#{id}")
+		gist.name = response["description"]
+		response["files"].each do |file|
+			text = file[1]["content"]
+		end
+		gist.content = RDiscount.new(text).to_html
+		gist.save
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gist::Application.config.secret_token = 'cc1a8dc27af1a4894c19c56fbf2189b35bc74edb9e8aef35f32eccf6a2679de6354f3609e69eb77d38ea4bf3bda99501ad34dbad8f8c126d6c857328d0e87a0c'
