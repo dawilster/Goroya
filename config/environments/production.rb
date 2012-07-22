d0eef6281fb01cc61293510f4aa9ccd62ecdf9ec
William Porter <dawilster143@gmail.com>
2012-07-22 16:35:46 +1000

    prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Trains::Application.configure do
+Gist::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
